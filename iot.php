576d7707125e2ffd9f8f01245369127e7a591db4
CreativeCoder9 <51862350+CreativeCoder9@users.noreply.github.com>
2019-07-07 06:14:51 +0530

    Done

---
+++ iot.php
@@ -0,0 +1,19 @@
+<?php 
+$file=fopen("data","w");
+$button=$_GET['onlight'];
+
+if(!empty($_GET))
+{
+	if ($button=="red"||$button=="yellow"||$button== "green"||$button=="reset") 
+	{
+		fwrite($file, $button);
+		fclose($file);
+		echo "SUCCESS";
+	}else{
+		 echo "ERROR.......NO_MATCH_FOUND";
+	} 
+}else{
+	echo "ERROR...ANOMALY_DETECTED";
+}
+
+?>
